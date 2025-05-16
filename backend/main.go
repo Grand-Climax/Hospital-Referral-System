5c598330f10fda3c6f1b6b64ba36de10c59fd02d
Kalki767 <kalkidankidane3@gmail.com>
2025-05-16 10:21:39 +0300

    Update main.go to include the role routing and start the server

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"Hospital-Referral-System/config"
+	"Hospital-Referral-System/internal/delivery/http/handlers"
+	router "Hospital-Referral-System/internal/delivery/http/routes"
 	"Hospital-Referral-System/internal/infrastructure/database"
+	"Hospital-Referral-System/internal/repository"
+	"Hospital-Referral-System/internal/usecase"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 	database.ConnectDB(cfg)
+
+	//setup the role api
+	rolerepo := repository.NewRoleRepository(database.DB)
+	roleusecase := usecase.NewRoleUsecase(rolerepo)
+	rolehandler := handlers.NewRoleHandler(roleusecase)
+	
+
+	r := router.SetupRouter(&router.RouterConfig{RoleHandler: rolehandler})
+
+	r.Run()
 }
