7531d02a8d3bc91f951485471087bcce8caab5bb
Kalki767 <kalkidankidane3@gmail.com>
2025-05-28 23:07:55 +0300

    Add additional methods on department repository interface

--- backend/internal/domain/contract/repository_interface/department_repository_interface.go
+++ backend/internal/domain/contract/repository_interface/department_repository_interface.go
@@ -4,6 +4,8 @@ import "Hospital-Referral-System/internal/domain/entity"
 
 type DepartmentRepositoryInterface interface {
 	CreateDepartment(department *entity.Department) (*entity.Department, error)
+	GetDepartmentByID(departmentid uint) (*entity.Department, error)
+	GetDepartmentByName(name string) (*entity.Department, error)
 	GetDepartments() ([]*entity.Department, error)
 	UpdateDepartment(updatedepartment *entity.Department) (*entity.Department, error) 
 	DeleteDepartment(departmentid uint) error
