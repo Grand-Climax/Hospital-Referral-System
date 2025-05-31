302cad28c39afa669b45e18031c991fdbf610927
Kalki767 <kalkidankidane3@gmail.com>
2025-05-31 10:53:04 +0300

    Update department handler implementation to use custom respond messages and fix bug which uses usecase implementation instead of the interface

--- backend/internal/delivery/http/handlers/department_handler.go
+++ backend/internal/delivery/http/handlers/department_handler.go
@@ -6,7 +6,6 @@ import (
 	usecase "Hospital-Referral-System/internal/domain/contract/usecase_interface"
 	"errors"
 	"strconv"
-	"strings"
 
 	"net/http"
 
@@ -32,14 +31,13 @@ func (handler *DepartmentHandler) CreateDepartment(ctx *gin.Context) {
 		return
 	}
 
-	department.Name = strings.ToLower(department.Name)
 	created, err := handler.depusecase.CreateDepartment(&department)
 	if err != nil {
 		if errors.Is(err, customerrors.ErrDuplicate){
-			ctx.IndentedJSON(http.StatusConflict, gin.H{"message": "department name already exists can't create a department with the given name", "error": err})
+			RespondErrors(ctx, http.StatusConflict, "department name already exists can't create a department with the given name", err)
 			return
 		}
-		ctx.IndentedJSON(http.StatusInternalServerError,gin.H{"message": "Something went wrong", "error": err})
+		RespondErrors(ctx, http.StatusInternalServerError, "Something went wrong", err)
 		return
 	}
 
@@ -49,7 +47,7 @@ func (handler *DepartmentHandler) CreateDepartment(ctx *gin.Context) {
 func (handler *DepartmentHandler) GetDepartments(ctx *gin.Context) {
 	departments, err := handler.depusecase.GetDepartments()
 	if err != nil {
-		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err})
+		RespondErrors(ctx, http.StatusInternalServerError, "Something Went wrong", err)
 		return
 	}
 	if len(departments) == 0{
@@ -62,18 +60,16 @@ func (handler *DepartmentHandler) GetDepartments(ctx *gin.Context) {
 func (handler *DepartmentHandler) GetDepartmentByName(ctx *gin.Context) {
 	name := ctx.Query("name")
 	if name == "" {
-		ctx.IndentedJSON(http.StatusUnprocessableEntity, gin.H{"message": "Name can not be empty", "error": "empty field"})
+		Respond(ctx, http.StatusUnprocessableEntity, "Name can not be empty")
 		return
 	}
-	name = strings.ToLower(name)
 	dep, err := handler.depusecase.GetDepartmentByName(name)
 	if err != nil {
 		if errors.Is(err, customerrors.ErrNotFound) {
-			ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "department with the given name is not found", "error": err})
+			RespondErrors(ctx, http.StatusNotFound, "department with the given name is not found", err)
 			return
 		}
-
-		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Something went wrong", "error": err})
+		RespondErrors(ctx, http.StatusInternalServerError, "Something went wrong", err)
 		return
 	}
 
@@ -82,28 +78,27 @@ func (handler *DepartmentHandler) GetDepartmentByName(ctx *gin.Context) {
 func (handler *DepartmentHandler) UpdateDepartment(ctx *gin.Context) {
 	var department entity.Department
 	if err := ctx.ShouldBindJSON(&department); err != nil {
-		ctx.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid input", "error": err})
+		RespondErrors(ctx, http.StatusBadRequest, "Invalid input", err)
 		return
 	}
 
 	if department.Name == ""{
-		ctx.IndentedJSON(http.StatusUnprocessableEntity, gin.H{"message": "Name field is empty", "error": "name cannot be empty"})
+		Respond(ctx, http.StatusUnprocessableEntity, "Name field is empty")
 		return
 	}
 
 	if department.ID == 0 {
-		ctx.IndentedJSON(http.StatusUnprocessableEntity, gin.H{"message": "Id field is not provided", "error": "Id is required"})
+		Respond(ctx, http.StatusUnprocessableEntity, "Id field is not provided")
 		return
 	}
 	
 	updatedDepartment, err := handler.depusecase.UpdateDepartment(&department)
 	if err != nil {
 		if errors.Is(err, customerrors.ErrNotFound){
-			ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "Department doesn't exist", "error": err})
+			RespondErrors(ctx, http.StatusNotFound, "Department doesn't exist", err)
 			return
 		}
-
-		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Couldn't update department", "error": err})
+		RespondErrors(ctx, http.StatusInternalServerError, "Couldn't update department", err)
 		return
 	}
 
@@ -113,26 +108,26 @@ func (handler *DepartmentHandler) UpdateDepartment(ctx *gin.Context) {
 func (handler *DepartmentHandler) DeleteDepartment(ctx *gin.Context) {
 	id := ctx.Param("id")
 	if id == "" {
-		ctx.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Id not provided", "error": "Id field cannot be empty"})
+		Respond(ctx, http.StatusBadRequest, "Id not provided")
 		return
 	}
 	dep_id, err := strconv.Atoi(id)
 	if err != nil {
-		ctx.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid id format", "error": err})
+		RespondErrors(ctx, http.StatusBadRequest, "Invalid id format", err)
 		return
 	}
 
 	if dep_id <= 0{
-		ctx.IndentedJSON(http.StatusUnprocessableEntity, gin.H{"message": "Id field is not positive", "error": "Id field cannot be non positive"})
+		Respond(ctx, http.StatusUnprocessableEntity, "Id field is not positive")
 		return
 	}
 	err = handler.depusecase.DeleteDepartment(uint(dep_id))
 	if err != nil {
 		if errors.Is(err, customerrors.ErrNotFound){
-			ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "department not found", "error": err})
+			RespondErrors(ctx, http.StatusNotFound, "department not found", err)
 			return
 		}
-		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Id not found", "error": err})
+		RespondErrors(ctx, http.StatusInternalServerError, "Id not found", err)
 		return
 	}
 
