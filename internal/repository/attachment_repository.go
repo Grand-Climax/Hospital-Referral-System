9439889ed12260d6ed2f0ad703b41b38854a2493
Kalki767 <kalkidankidane3@gmail.com>
2026-03-14 17:03:58 +0300

    refactor(repository): align attachment repository with domain interface
    - Update `attachmentRepository` to implement the `irepository.AttachmentRepository` interface.
    - Embed the generic `BaseRepository` to provide standard CRUD functionality.
    - This change ensures consistency with the new architectural design.

--- internal/repository/attachment_repository.go
+++ internal/repository/attachment_repository.go
@@ -4,18 +4,15 @@ import (
 	"gorm.io/gorm"
 
 	"Hospital-Referral-System/internal/domain/entity"
+	irepository "Hospital-Referral-System/internal/domain/interfaces/repository"
 )
 
-type AttachmentRepository interface {
-	BaseRepository[entity.Attachment]
-}
-
 type attachmentRepository struct {
-	BaseRepository[entity.Attachment]
+	*BaseRepository[entity.Attachment]
 	db *gorm.DB
 }
 
-func NewAttachmentRepository(db *gorm.DB) AttachmentRepository {
+func NewAttachmentRepository(db *gorm.DB) irepository.AttachmentRepository {
 	return &attachmentRepository{
 		BaseRepository: NewBaseRepository[entity.Attachment](db),
 		db:             db,
